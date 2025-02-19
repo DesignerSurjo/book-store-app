3e18faf6630d95bfedd752cd62b62114aaae00d1
DesignerSurjo <techguru6193@gmail.com>
2025-02-19 20:43:47 +0600

    first change

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -20,7 +20,7 @@ const Search = () => {
    
   return showSearch && visible ? (
     <div className='border-t border-b border-gray-200 bg-gray-50 text-center'>
-    <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
+    <div className='inline-flex items-center justify-center border border-gray-400 px-3 py-2 my-5 mx-2 rounded-full w-3/4 sm:w-1/2'>
         <input value={search} onChange={(e) => setSearch(e.target.value)} className='flex-1 border-gray-100 outline-none bg-inherit text-sm' type="text" placeholder='Search Here....' />
         <img src={assets.search_icon} alt="" className='w-4' />
     </div>
