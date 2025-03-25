64935813c36264244d76686b6a655b6596c0868b
DesignerSurjo <techguru6193@gmail.com>
2025-03-26 03:54:30 +0600

    Some bug fix

--- frontend/src/components/CouponApply.jsx
+++ frontend/src/components/CouponApply.jsx
@@ -6,15 +6,15 @@ import { ShopContext } from '../context/ShopContext';
 const CouponApply = ({ setDiscount, cartTotal }) => {
   const [couponCode, setCouponCode] = useState('');
   const { backendUrl, token } = useContext(ShopContext); // Context থেকে token নিচ্ছি
-
+  const [showInput, setShowInput] = useState(false); // ইনপুট দেখানোর স্টেট
   const applyCouponHandler = async () => {
     if (!couponCode) {
-      toast.error('⚠️ Please enter a coupon code!');
+      toast.error('⚠️অনুগ্রহ করে কুপন কোড লিখুন!');
       return;
     }
 
     if (!token) {
-      toast.error('❌ Please log in to apply a coupon!');
+      toast.error('❌ কুপন অ্যাপ্লাই করতে লগইন করুন!');
       return;
     }
 
@@ -34,32 +34,43 @@ const CouponApply = ({ setDiscount, cartTotal }) => {
           : discountAmount;  
 
         setDiscount(Number(calculatedDiscount));
-        toast.success(`🎉 Coupon applied! Discount: ৳${calculatedDiscount.toFixed(2)}`);
+        toast.success(`🎉 কুপন সফলভাবে অ্যাপ্লাই হয়েছে! ছাড়: ৳${finalDiscount.toFixed(2)}`);
         setCouponCode('');  
       } else {
         toast.error(res.data.message); 
       }
     } catch (error) {
-      toast.error(error.response?.data?.message || '❌ Server Error');
+      toast.error(error.response?.data?.message || '❌ সার্ভার ত্রুটি হয়েছে!');
     }
   };
 
   return (
     <div className="mt-4">
-      <input
-        type="text"
-        value={couponCode}
-        onChange={(e) => setCouponCode(e.target.value)}
-        placeholder="Enter Coupon Code"
-        className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
-      />
-      <button
-        onClick={applyCouponHandler}
-        type="button"
-        className="bg-[#C31C37] cursor-pointer rounded-md text-white px-4 py-2 mt-2"
-      >
-        Apply Coupon
-      </button>
+      {!showInput ? (
+        <p 
+          onClick={() => setShowInput(true)}
+          className="text-[#C31C37] cursor-pointer underline"
+        >
+          আপনার কাছে কি কুপন আছে?
+        </p>
+      ) : (
+        <>
+          <input
+            type="text"
+            value={couponCode}
+            onChange={(e) => setCouponCode(e.target.value)}
+            placeholder="কুপন কোড লিখুন"
+            className="border border-gray-300 rounded py-1.5 px-3.5 w-full"
+          />
+          <button
+            onClick={applyCouponHandler}
+            type="button"
+            className="bg-[#C31C37] cursor-pointer rounded-md text-white px-4 py-2 mt-2"
+          >
+            কুপন ব্যবহার করুন
+          </button>
+        </>
+      )}
     </div>
   );
 };
